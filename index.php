645a0cd208e5de93818c6a35884aa300f63eefbf
janhr <xhrcan@stuba.sk>
2022-05-19 17:11:38 +0200

    Kostra layoutu

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 ?>
 
+<!-- Dizajn trochu inspirovany pracou pana Mateja Rabeka -->
 <!doctype html>
 <html lang="en">
 <head>
@@ -9,9 +10,24 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <link rel="icon" type="image/x-icon" href="./fei.png">
+    <link rel="stylesheet" href="./css/style.css">
+    <script src="js/script.js" defer></script>
+    <title>FinalCountdown</title>
 </head>
+
 <body>
+<br><br><br>
+<div class="container">
+    <h1>Animácia dynamického systému „tlmič automobil“</h1>
+    <form class="marginBottom">
+        <label for="test"></label>
+        <input id="test" type="text">
+        <button type="submit">Submit</button>
+    </form>
+    <p>&copy; Ivan Cicka, Jan Hrćan, Jovan Kiš, Paljko Urbanek</p>
+</div>
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
 
 </body>
 </html>
