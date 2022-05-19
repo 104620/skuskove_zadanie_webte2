b7a4237f2258e5d62f2888f7523dbf66641b7861
janhr <xhrcan@stuba.sk>
2022-05-19 18:33:56 +0200

    Form done

--- index.php
+++ index.php
@@ -29,10 +29,57 @@
 
 <div class="container">
     <h1>Animácia dynamického systému „tlmič automobil“</h1>
-    <form class="marginBottom">
-        <label for="test">Test</label>
-        <input id="test" type="text">
-        <button type="submit">Submit</button>
+    <form class="marginBottom" action="index.php" method="post">
+
+        <div class="grid">
+            <div>
+                <label class="label" for="k1">k1</label>
+                <input id="k1" type="text">
+            </div>
+            <div>
+                <label class="label" for="k2">k2</label>
+                <input id="k2" type="text">
+            </div>
+            <div>
+                <label class="label" for="k3">k3</label>
+                <input id="k3" type="text">
+            </div>
+        </div>
+        <div class="grid">
+            <div>
+                <label class="label" for="f1">F1</label>
+                <input id="f1" type="text">
+            </div>
+            <div>
+                <label class="label" for="f2">F2</label>
+                <input id="f2" type="text">
+            </div>
+        </div>
+        <div class="grid">
+            <div>
+                <label class="label" for="m1">m1</label>
+                <input id="m1" type="text">
+            </div>
+            <div>
+                <label class="label" for="m2">m2</label>
+                <input id="m2" type="text">
+            </div>
+        </div>
+        <br>
+
+        <div class="marginBottom">
+            <input type="checkbox" id="graph" name="graph" value="Graf">
+            <label for="graph"> Zobraziť graf</label><br>
+            <input type="checkbox" id="anim" name="anim" value="Animation">
+            <label for="anim"> Zobraziť animáciu</label><br>
+        </div>
+
+        <button type="submit" class="marginBottom">Submit</button>
+
+        <div id="div">
+            <div id="graphDiv"></div>
+            <div id="animDiv"></div>
+        </div>
     </form>
     <p>&copy; Ivan Cicka, Jan Hrćan, Jovan Kiš, Paljko Urbanek</p>
 </div>
