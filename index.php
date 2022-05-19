1ce142c1ba0528842ed515b51b2d290524de9fda
janhr <xhrcan@stuba.sk>
2022-05-19 18:45:16 +0200

    Language done

--- index.php
+++ index.php
@@ -34,47 +34,47 @@
         <div class="grid">
             <div>
                 <label class="label" for="k1">k1</label>
-                <input id="k1" type="text">
+                <input id="k1" type="number">
             </div>
             <div>
                 <label class="label" for="k2">k2</label>
-                <input id="k2" type="text">
+                <input id="k2" type="number">
             </div>
             <div>
                 <label class="label" for="k3">k3</label>
-                <input id="k3" type="text">
+                <input id="k3" type="number">
             </div>
         </div>
         <div class="grid">
             <div>
                 <label class="label" for="f1">F1</label>
-                <input id="f1" type="text">
+                <input id="f1" type="number">
             </div>
             <div>
                 <label class="label" for="f2">F2</label>
-                <input id="f2" type="text">
+                <input id="f2" type="number">
             </div>
         </div>
         <div class="grid">
             <div>
                 <label class="label" for="m1">m1</label>
-                <input id="m1" type="text">
+                <input id="m1" type="number">
             </div>
             <div>
                 <label class="label" for="m2">m2</label>
-                <input id="m2" type="text">
+                <input id="m2" type="number">
             </div>
         </div>
         <br>
 
         <div class="marginBottom">
             <input type="checkbox" id="graph" name="graph" value="Graf">
-            <label for="graph"> Zobraziť graf</label><br>
+            <label for="graph" id="graphLabel"></label><br>
             <input type="checkbox" id="anim" name="anim" value="Animation">
-            <label for="anim"> Zobraziť animáciu</label><br>
+            <label for="anim" id="animLabel"></label><br>
         </div>
 
-        <button type="submit" class="marginBottom">Submit</button>
+        <button type="submit" class="marginBottom" id="button"></button>
 
         <div id="div">
             <div id="graphDiv"></div>
