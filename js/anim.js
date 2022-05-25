7b37910650edb4aa94b52c924ee27514b2051b1e
janhr <xhrcan@stuba.sk>
2022-05-25 12:55:41 +0200

    Animation without loop

--- js/anim.js
+++ js/anim.js
@@ -1,5 +1,6 @@
 const width = 400;                  // Width of the canvas
 const height = 300;                 // Height of the canvas
+let url1  = 'https://site51.webte.fei.stuba.sk/zaverecne/OctaveGeneratedFiles/xyt.json';
 
 // Draw
 const draw = SVG()
@@ -9,8 +10,15 @@ const draw = SVG()
 
 let canvas = draw.group();
 
-let lines1 = [];
-let lines2 = [];
+// Elements
+// let lines1 = [];
+// let lines2 = [];
+let lines = [];
+let upperLine;
+let m1;
+let m2;
+let m1Text;
+let m2Text;
 
 drawCanvas(canvas);
 drawMovable(canvas);
@@ -46,41 +54,96 @@ function drawCanvas(canvas){
 
 function drawMovable(canvas){
 
+    // Whole line
+    lines.push(canvas.polyline('10, 120 20, 100 30, 120 40, 100 50, 120 60, 100 70, 120 80, 100' +
+        ' 90, 120 100, 100 110, 120 120, 100 130, 100').fill('none').stroke({color: 'black', width: 2}));
+    lines.push(canvas.polyline('170, 120 180, 100 190, 120 200, 100 210, 120 220, 100 230, 120 240, 100 250, 120')
+        .fill('none').stroke({color: 'black', width: 2}));
+
     // Upper line
-    canvas.polyline('10, 80 250, 80').fill('none').stroke({color: 'black', width: 2});
+    upperLine = canvas.polyline('10, 80 250, 80').fill('none').stroke({color: 'black', width: 2});
 
     // Lower line
-    lines1.push(canvas.polyline('10, 120 20, 100').fill('none').stroke({color: 'black', width: 2}));
-    lines1.push(canvas.polyline('20, 100 30, 120').fill('none').stroke({color: 'black', width: 2}));
-    lines1.push(canvas.polyline('30, 120 40, 100').fill('none').stroke({color: 'black', width: 2}));
-    lines1.push(canvas.polyline('40, 100 50, 120').fill('none').stroke({color: 'black', width: 2}));
-    lines1.push(canvas.polyline('50, 120 60, 100').fill('none').stroke({color: 'black', width: 2}));
-    lines1.push(canvas.polyline('60, 100 70, 120').fill('none').stroke({color: 'black', width: 2}));
-    lines1.push(canvas.polyline('70, 120 80, 100').fill('none').stroke({color: 'black', width: 2}));
-    lines1.push(canvas.polyline('80, 100 90, 120').fill('none').stroke({color: 'black', width: 2}));
-    lines1.push(canvas.polyline('90, 120 100, 100').fill('none').stroke({color: 'black', width: 2}));
-    lines1.push(canvas.polyline('100, 100 110, 120').fill('none').stroke({color: 'black', width: 2}));
-    lines1.push(canvas.polyline('110, 120 120, 100').fill('none').stroke({color: 'black', width: 2}));
-    lines1.push(canvas.polyline('120, 100 130, 120').fill('none').stroke({color: 'black', width: 2}));
-
-    lines2.push(canvas.polyline('170, 120 180, 100').fill('none').stroke({color: 'black', width: 2}));
-    lines2.push(canvas.polyline('180, 100 190, 120').fill('none').stroke({color: 'black', width: 2}));
-    lines2.push(canvas.polyline('190, 120 200, 100').fill('none').stroke({color: 'black', width: 2}));
-    lines2.push(canvas.polyline('200, 100 210, 120').fill('none').stroke({color: 'black', width: 2}));
-    lines2.push(canvas.polyline('210, 120 220, 100').fill('none').stroke({color: 'black', width: 2}));
-    lines2.push(canvas.polyline('220, 100 230, 120').fill('none').stroke({color: 'black', width: 2}));
-    lines2.push(canvas.polyline('230, 120 240, 100').fill('none').stroke({color: 'black', width: 2}));
-    lines2.push(canvas.polyline('240, 100 250, 120').fill('none').stroke({color: 'black', width: 2}));
+    // lines1.push(canvas.polyline('10, 120 20, 100').fill('none').stroke({color: 'black', width: 2}));
+    // lines1.push(canvas.polyline('20, 100 30, 120').fill('none').stroke({color: 'black', width: 2}));
+    // lines1.push(canvas.polyline('30, 120 40, 100').fill('none').stroke({color: 'black', width: 2}));
+    // lines1.push(canvas.polyline('40, 100 50, 120').fill('none').stroke({color: 'black', width: 2}));
+    // lines1.push(canvas.polyline('50, 120 60, 100').fill('none').stroke({color: 'black', width: 2}));
+    // lines1.push(canvas.polyline('60, 100 70, 120').fill('none').stroke({color: 'black', width: 2}));
+    // lines1.push(canvas.polyline('70, 120 80, 100').fill('none').stroke({color: 'black', width: 2}));
+    // lines1.push(canvas.polyline('80, 100 90, 120').fill('none').stroke({color: 'black', width: 2}));
+    // lines1.push(canvas.polyline('90, 120 100, 100').fill('none').stroke({color: 'black', width: 2}));
+    // lines1.push(canvas.polyline('100, 100 110, 120').fill('none').stroke({color: 'black', width: 2}));
+    // lines1.push(canvas.polyline('110, 120 120, 100').fill('none').stroke({color: 'black', width: 2}));
+    // lines1.push(canvas.polyline('120, 100 130, 120').fill('none').stroke({color: 'black', width: 2}));
+    //
+    // lines2.push(canvas.polyline('170, 120 180, 100').fill('none').stroke({color: 'black', width: 2}));
+    // lines2.push(canvas.polyline('180, 100 190, 120').fill('none').stroke({color: 'black', width: 2}));
+    // lines2.push(canvas.polyline('190, 120 200, 100').fill('none').stroke({color: 'black', width: 2}));
+    // lines2.push(canvas.polyline('200, 100 210, 120').fill('none').stroke({color: 'black', width: 2}));
+    // lines2.push(canvas.polyline('210, 120 220, 100').fill('none').stroke({color: 'black', width: 2}));
+    // lines2.push(canvas.polyline('220, 100 230, 120').fill('none').stroke({color: 'black', width: 2}));
+    // lines2.push(canvas.polyline('230, 120 240, 100').fill('none').stroke({color: 'black', width: 2}));
+    // lines2.push(canvas.polyline('240, 100 250, 120').fill('none').stroke({color: 'black', width: 2}));
 
     // Masses
-    canvas.rect(40, 60)
+    m1 = canvas.rect(40, 60)
         .move(130, 70)
         .fill("#5a52c9")
         .stroke({ width: 2, color: 'black' })
-    canvas.rect(40, 60)
+    m2 = canvas.rect(40, 60)
         .move(250, 70)
         .fill("#cc2a2a")
         .stroke({ width: 2, color: 'black' })
-    canvas.text("m1").move(140, 110).font({size: 20}).fill("white");
-    canvas.text("m2").move(260, 110).font({size: 20}).fill("white");
+    m1Text = canvas.text("m1").move(140, 110).font({size: 20}).fill("white");
+    m2Text = canvas.text("m2").move(260, 110).font({size: 20}).fill("white");
+}
+
+animation();
+
+function animation(){
+    fetch(url1)
+        .then(res => res.json())
+        .then((out) => {
+            let x1 = [];
+            let x2 = [];
+            for(let i = 0; i < out.data[0].length; i++) {
+                x1.push(out.data[0][i][1])
+                x2.push(out.data[0][i][3])
+            }
+
+            let xValues = [];
+            let yValues = [];
+
+            for(let i = 1; i < x1.length; i++){
+                xValues.push(130 + 20 * x1[i]);
+                yValues.push(250 + 20 * x2[i]);
+            }
+
+            // Create timelines
+
+            for(let j = 0; j < xValues.length; j++) {
+                let block1 = xValues[j] / 12;
+                let block2 = (yValues[j] - (xValues[j] + 40)) / 8;
+
+                m1.animate({duration: 10}).move(xValues[j], 70);
+                m1Text.animate({duration: 10}).move(xValues[j] + 10, 100);
+                m2.animate({duration: 10}).move(yValues[j], 70);
+                m2Text.animate({duration: 10}).move(yValues[j] + 10, 100);
+                upperLine.animate(10).plot([[10, 80], [yValues[j], 80]]);
+                lines[0].animate(10)
+                    .plot([[10, 120], [block1 + 10, 100], [block1 * 2 + 10, 120], [block1 * 3 + 10, 100],
+                          [block1 * 4 + 10, 120], [block1 * 5 + 10, 100], [block1 * 6 + 10, 120],
+                          [block1 * 7 + 10, 100], [block1 * 8 + 10, 120], [block1 * 9 + 10, 100],
+                          [block1 * 10 + 10, 120], [block1 * 11 + 10, 100], [xValues[j], 120]]);
+                lines[1].animate(10)
+                    .plot([[xValues[j] + 40, 120], [block2 + xValues[j] + 40, 100], [block2 * 2 + xValues[j] + 40, 120],
+                        [block2 * 3 + xValues[j] + 40, 100],
+                        [block2 * 4 + xValues[j] + 40, 120], [block2 * 5 + xValues[j] + 40, 100],
+                        [block2 * 6 + xValues[j] + 40, 120],
+                        [block2 * 7 + xValues[j] + 40, 100], [yValues[j], 120]]);
+            }
+
+        })
+        .catch(err => { throw err });
 }
