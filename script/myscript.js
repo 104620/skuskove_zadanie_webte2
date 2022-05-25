5593c122c1f5c9282ddb7cded5b2f15de146399b
Palko <xurbanek@stuba.sk>
2022-05-25 20:29:23 +0200

    plot with animation

--- script/myscript.js
+++ script/myscript.js
@@ -8,6 +8,8 @@ function plotDataChart() {
             GRAPH = document.getElementById("dataChart");
             x1 = []
             x3 = []
+            var frames = []
+
             for(let i = 0; i < out.data[0].length; i++) {
                 x1.push(out.data[0][i][1])
                 x3.push(out.data[0][i][3])
@@ -28,10 +30,11 @@ function plotDataChart() {
                 {
                     x: out.data[2],
                     y: x1,
+                    mode: 'lines',
                     name: "car",
                     line:
                         {
-                            width: 3,
+                            width: 4,
                             shape: "spline",
                             color: "#90ee90"
                         },
@@ -40,15 +43,16 @@ function plotDataChart() {
                 {
                     x: out.data[2],
                     y: x3,
+                    mode: 'lines',
                     name: "wheel",
                     line:
                         {
-                            width: 3,
+                            width: 4,
                             shape: "spline",
                             color: "#008080"
                         },
                     visible: true
-                }
+                },
             ]
 
             let config = {
@@ -77,52 +81,42 @@ function plotDataChart() {
 
             };
 
-            console.log(data)
+            var n = out.data[2].length;
+            for (var i = 0; i < n; i++) {
+                frames[i] = {data: [{x: [], y: []}, {x: [], y: []}]}
+                frames[i].data[0].x = out.data[2].slice(0, i+1);
+                frames[i].data[0].y = x1.slice(0, i+1);
+                frames[i].data[1].x = out.data[2].slice(0, i+1);
+                frames[i].data[1].y = x3.slice(0, i+1);
+            }
 
-            Plotly.newPlot(GRAPH, data, layout, config);
+            Plotly.newPlot(GRAPH, data, layout, config)
 
-            // function compute () {
-            //     var s = 10, b = 8/3, r = 28;
-            //     var dx, dy, dz;
-            //     var xh, yh, zh;
-            //     for (var i = 0; i < n; i++) {
-            //         dx = s * (y[i] - x[i]);
-            //         dy = x[i] * (r - z[i]) - y[i];
-            //         dz = x[i] * y[i] - b * z[i];
-            //
-            //         xh = x[i] + dx * dt * 0.5;
-            //         yh = y[i] + dy * dt * 0.5;
-            //         zh = z[i] + dz * dt * 0.5;
-            //
-            //         dx = s * (yh - xh);
-            //         dy = xh * (r - zh) - yh;
-            //         dz = xh * yh - b * zh;
-            //
-            //         x[i] += dx * dt;
-            //         y[i] += dy * dt;
-            //         z[i] += dz * dt;
-            //     }
-            // }
-            //
-            // function update () {
-            //     compute();
-            //
-            //     Plotly.animate('myDiv', {
-            //         data: [{x: x, y: z}]
-            //     }, {
-            //         transition: {
-            //             duration: 0
-            //         },
-            //         frame: {
-            //             duration: 0,
-            //             redraw: false
-            //         }
-            //     });
-            //
-            //     requestAnimationFrame(update);
-            // }
-            //
-            // requestAnimationFrame(update);
+            Plotly.animate(GRAPH, frames, {
+                transition: {
+                    duration: 10,
+                    easing: 'linear'
+                },
+                frame: {
+                    duration: 10,
+                    redraw: false,
+                },
+            });
+
+            setInterval(function() {
+                Plotly.animate(GRAPH, frames, {
+                    transition: {
+                        duration: 10,
+                        easing: 'linear'
+                    },
+                    frame: {
+                        duration: 10,
+                        redraw: false,
+                    },
+                    // mode: mode
+                });
+            },5000)
+
         })
         .catch(err => { throw err });
 }
